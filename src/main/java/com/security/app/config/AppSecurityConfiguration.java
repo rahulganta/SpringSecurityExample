97e7d966eff48b583dc43984ff02701ab23527e7
rahulganta <gantarahul@gmail.com>
2019-09-25 15:41:11 -0400

    form based security and login and logout implemented

---
+++ src/main/java/com/security/app/config/AppSecurityConfiguration.java
@@ -0,0 +1,48 @@
+package com.security.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/index", "/css/*", "/js/*").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .and()
+                .logout().invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/logout-success").permitAll();
+
+
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        List<UserDetails> users = new ArrayList<>();
+        users.add(User.withDefaultPasswordEncoder().username("rahul").password("password").roles("USER","ADMIN").build());
+        users.add(User.withDefaultPasswordEncoder().username("suchi").password("ganta").roles("USER").build());
+        return new InMemoryUserDetailsManager(users);
+    }
+}
---
+++ src/main/java/com/security/app/controller/Home.java
@@ -0,0 +1,32 @@
+package com.security.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class Home {
+
+    @RequestMapping(value = {"/", "/home", "/index"}, method = RequestMethod.GET)
+    public String home(Model model) {
+        return "index";
+    }
+
+    @RequestMapping(value = "employees", method = RequestMethod.GET)
+    public String employeesList(Model model) {
+        return "employees";
+    }
+
+    @GetMapping(value="/login")
+    public String login(Model model) {
+        return "login";
+    }
+
+    @GetMapping(value = "/logout-success")
+    public String logout() {
+        return "logout";
+    }
+}
