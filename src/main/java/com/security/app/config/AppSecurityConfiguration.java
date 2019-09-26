5a79c87a1ef95b6daacab13c47bfe74f8b6b1f32
rahulganta <gantarahul@gmail.com>
2019-09-25 22:29:14 -0400

    login form and dependency changes

--- src/main/java/com/security/app/config/AppSecurityConfiguration.java
+++ src/main/java/com/security/app/config/AppSecurityConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.util.matcher.AndRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.ArrayList;
